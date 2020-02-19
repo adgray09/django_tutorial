5b476b51f0c47980fca14fecb57314aa9a0d612f
Razor <alexandergray37@gmail.com>
2020-02-18 16:14:06 -0800

    more code

--- polls/urls.py
+++ polls/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('form/', views.form_demo, name="form-demo"), # polls/form/
+    path('form/questions', views.QuestionCreateView.as_view(), name="new-question-form")
 ]
